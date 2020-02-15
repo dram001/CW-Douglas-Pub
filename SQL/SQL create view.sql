3e3913ca92a4f01f8a46f5b6599bda0ab22e10b2
Douglas Ho <dhks11@gmail.com>
2020-02-16 06:10:48 +0800

    updated README instruction for runing the project

---
+++ SQL/SQL create view.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[Sales]
+AS SELECT Orders.OrderID, 
+          SUM(OrderDetails.Quantity * Items.Price) AS TotalSales
+   FROM OrderDetails, 
+        Items, 
+        Orders
+   WHERE Orders.OrderID = OrderDetails.OrderID AND 
+         OrderDetails.ItemId = Items.ID
+   GROUP BY Orders.OrderID;
